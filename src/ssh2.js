450650ad60b691fb37422fa6a3e64bb0d1dd1dfa
unknown <bansal.rahul14@gmail.com>
2020-12-08 16:27:20 +0530

    removed test connection

--- src/ssh2.js
+++ src/ssh2.js
@@ -1,15 +1,6 @@
-const { readFileSync } = require('fs');
-
 const { Client } = require('ssh2');
 const { getSSHConnectionObj } = require('./sshService')
 const conn = new Client();
-
-const testConnection = {
-  host: '13.127.110.225',
-  port: 22,
-  username: 'a3052st2wlfjtacfdwgru3u7melltc8',
-  privateKey: readFileSync('C:/Users/rabans/Polymesh/.stormy/.ssh/id_rsa')
-}
 
 const executeRemote = (cmd, uuid, outputCb) => {
   return new Promise( (resolve, reject) => {
